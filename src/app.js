6562e8cfe8168b3fe7fedc5d7f4146989853d77d
George Martinez <gemartinez@fi.uba.ar>
2022-07-10 19:46:11 -0300

    v1.0.1

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import path from 'path'
 const app = express()
 
+/* 
 import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
+import { fileURLToPath } from 'url'; 
 const currentDirectory = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(currentDirectory, 'public')));
+*/
 
-app.use(express.static(path.join(currentDirectory, 'public')))
-//
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 export default app
